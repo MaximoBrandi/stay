e80d99372042e4b26b9047e7b3cfb183a3810fe9
MaximoBrandi <maximoprandi@gmail.com>
2023-07-03 18:28:23 -0300

    seventh commit '5' - PUSH TAG US0e3DGZ

--- resources/views/database.blade.php
+++ resources/views/database.blade.php
@@ -8,40 +8,57 @@
     <link rel="stylesheet" href="vendor/hystModal/hystmodal.min.css">
     <script src="vendor/hystModal/hystmodal.min.js"></script>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.5/flowbite.min.js"></script>
-
-
     <div class="py-12">
-            @if (Auth::user()->privilege->privilege_grade == 4)
-                @foreach (App\Models\Team::all() as $value)
-                <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div id="accordion-collapse" data-accordion="collapse">
-                        <h2 id="accordion-collapse-heading-{{$value->id}}">
-                        <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-{{$value->id}}" aria-expanded="false" aria-controls="accordion-collapse-body-{{$value->id}}">
-                            <span>Course {{ $value->id }}</span>
-                            <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                        </button>
-                        </h2>
-                        <div id="accordion-collapse-body-{{$value->id}}" class="hidden" aria-labelledby="accordion-collapse-heading-{{$value->id}}">
-                            <livewire:database-course :course=" $value->id " />
+        @if (Auth::user()->privilege->privilege_grade == 4)
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div id="accordionExample5">
+                    @foreach (App\Models\Team::all() as $value)
+                        <div class="border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-neutral-800">
+                            <h2 class="mb-0" id="headingTwo{{ $value->id }}">
+                                <button
+                                    class="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-neutral-800 dark:text-white [&:not([data-te-collapse-collapsed])]:bg-white [&:not([data-te-collapse-collapsed])]:text-primary [&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(229,231,235)] dark:[&:not([data-te-collapse-collapsed])]:bg-neutral-800 dark:[&:not([data-te-collapse-collapsed])]:text-primary-400 dark:[&:not([data-te-collapse-collapsed])]:[box-shadow:inset_0_-1px_0_rgba(75,85,99)]"
+                                    type="button" data-te-collapse-init data-te-collapse-collapsed data-te-target="#collapseTwo{{ $value->id }}"
+                                    aria-expanded="false" aria-controls="collapseTwo{{ $value->id }}">
+                                    {{ $value->name }}
+                                    <span
+                                        class="-mr-1 ml-auto h-5 w-5 shrink-0 rotate-[-180deg] fill-[#336dec] transition-transform duration-200 ease-in-out group-[[data-te-collapse-collapsed]]:mr-0 group-[[data-te-collapse-collapsed]]:rotate-0 group-[[data-te-collapse-collapsed]]:fill-[#212529] motion-reduce:transition-none dark:fill-blue-300 dark:group-[[data-te-collapse-collapsed]]:fill-white">
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+                                            stroke="currentColor" class="h-6 w-6">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+                                    </span>
+                                </button>
+                            </h2>
+                            <div id="collapseTwo{{ $value->id }}" class="!visible hidden" data-te-collapse-item aria-labelledby="headingTwo{{ $value->id }}">
+                                <livewire:database-course :course="$value->id" />
+                            </div>
                         </div>
-                    </div>
+                    @endforeach
                 </div>
-                @endforeach
-            @else
+            </div>
+        @else
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
                 <div id="accordion-collapse" data-accordion="collapse">
                     <h2 id="accordion-collapse-heading-1">
-                    <button type="button" class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800" data-accordion-target="#accordion-collapse-body-1" aria-expanded="false" aria-controls="accordion-collapse-body-1">
-                        <span>Course {{ Auth::user()->current_team_id }}</span>
-                        <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
-                    </button>
+                        <button type="button"
+                            class="flex items-center justify-between w-full p-5 font-medium text-left text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800"
+                            data-accordion-target="#accordion-collapse-body-1" aria-expanded="false"
+                            aria-controls="accordion-collapse-body-1">
+                            <span>Course {{ Auth::user()->current_team_id }}</span>
+                            <svg data-accordion-icon class="w-6 h-6 rotate-180 shrink-0" fill="currentColor"
+                                viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                                <path fill-rule="evenodd"
+                                    d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z"
+                                    clip-rule="evenodd"></path>
+                            </svg>
+                        </button>
                     </h2>
                     <div id="accordion-collapse-body-1" class="hidden" aria-labelledby="accordion-collapse-heading-1">
-                        <livewire:database-course :course=" Auth::user()->current_team_id " />
+                        <livewire:database-course :course="Auth::user()->current_team_id" />
                     </div>
                 </div>
             </div>
-            @endif
+        @endif
     </div>
+    <script src="https://cdn.jsdelivr.net/npm/tw-elements/dist/js/tw-elements.umd.min.js"></script>
 </x-app-layout>
--- resources/views/livewire/database-course.blade.php
+++ resources/views/livewire/database-course.blade.php
@@ -1,4 +1,4 @@
-<div class="bg-white dark:bg-gray-800 mt-8 overflow-hidden shadow-xl sm:rounded-lg">
+<div class="bg-white dark:bg-gray-800 overflow-hidden shadow-xl sm:rounded-lg">
     <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-20">
         <div class="mb-8 max-w-xl md:mx-auto sm:text-center lg:max-w-2xl">
             <h2 class="max-w-lg font-sans dark:text-gray-200 text-3xl font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto">
